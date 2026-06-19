bd663e1df15dc3389c374cbff5920aebd50a8064
Agustina-Aros <agustinaaros38@gmail.com>
2026-06-19 14:50:01 -0300

    Agregando css al menú en productos.css

--- catalogo.php
+++ catalogo.php
@@ -1,5 +1,5 @@
 <?php
-include("conexion.php");
+include("php/conexion.php");
 
 $resultado = $conexion->query(
     "SELECT * FROM productos"
@@ -37,7 +37,7 @@ $resultado = $conexion->query(
                         <li class="li2"><span class="material-symbols-outlined">deceased</span><a href="especial.html">Ediciones especiales</a></li>
                     </ul></li>
                     <li class="li1"><a href="favoritos.html">Favoritos</a></li>
-                    <li class="li1"><a href="catalogo.html">Catalogo</a></li>
+                    <li class="li1"><a href="catalogo.php">Catalogo</a></li>
                     <li class="li1"><a href="carrito.html">Carrito</a></li>
                 </ul>
             </div>  
@@ -48,7 +48,9 @@ $resultado = $conexion->query(
         <div class="titulo">
             <h1>Catálogo de Productos</h1>
         </div> 
-        <button id="btnAgregar">Agregar Producto</button>
+        <a href="PHP/agregar.php">
+            <button id="btnAgregar">Agregar Producto</button>
+        </a>
         <table>
             <thead>
                 <tr>
@@ -57,22 +59,24 @@ $resultado = $conexion->query(
                     <th>Stock</th>
                     <th>Acciones</th>
                 </tr>
+            </thead>
+            <tbody>
                 
                 <?php while($p = $resultado->fetch_assoc()){ ?>
                 <tr>
                     <td><?= $p['nombre'] ?></td>
                     <td>$<?= $p['precio'] ?></td>
                     <td><?= $p['stock'] ?></td>
-                    <td><a href="editar.php?id=<?= $p['id'] ?>">
-                        <button>Actualizar</button></a>
-                        <a href="eliminar.php?id=<?= $p['id'] ?>">
-                        <button>Eliminar</button></a>
+                    <td><a href="PHP/editar.php?id=<?= $p['id'] ?>">
+                            <button>Actualizar</button>
+                        </a>
+                        <a href="PHP/eliminar.php?id=<?= $p['id'] ?>">
+                            <button>Eliminar</button>
+                        </a>
                     </td>
                 </tr>
                 <?php } ?>
 
-            </thead>
-            <tbody id="tablaProductos">
             </tbody>
         </table>
 
