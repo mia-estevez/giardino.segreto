49af1e0df10fc917e0dfe67604367528707b13aa
Agustina-Aros <agustinaaros38@gmail.com>
2026-06-19 12:14:30 -0300

    Modificando catalogo.html a php

--- catalogo.php
+++ catalogo.php
@@ -1,3 +1,11 @@
+<?php
+include("conexion.php");
+
+$resultado = $conexion->query(
+    "SELECT * FROM productos"
+);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,6 +57,20 @@
                     <th>Stock</th>
                     <th>Acciones</th>
                 </tr>
+                
+                <?php while($p = $resultado->fetch_assoc()){ ?>
+                <tr>
+                    <td><?= $p['nombre'] ?></td>
+                    <td>$<?= $p['precio'] ?></td>
+                    <td><?= $p['stock'] ?></td>
+                    <td><a href="editar.php?id=<?= $p['id'] ?>">
+                        <button>Actualizar</button></a>
+                        <a href="eliminar.php?id=<?= $p['id'] ?>">
+                        <button>Eliminar</button></a>
+                    </td>
+                </tr>
+                <?php } ?>
+
             </thead>
             <tbody id="tablaProductos">
             </tbody>
